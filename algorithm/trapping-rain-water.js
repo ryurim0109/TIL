1ace5a5df6dbe7d0606032faf19d021b762cf658
ryurim0109 <dbfla5036@naver.com>
2022-08-01 21:08:04 +0900

    Update trapping-rain-water.js

---
+++ algorithm/trapping-rain-water.js
@@ -0,0 +1,9 @@
+const trap = (height) => {
+  const len = height.length;
+  let stack = [];
+  let answer = 0;
+  for (let i = 0; i < len; i++) {}
+};
+
+const height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+console.log(trap(height));
